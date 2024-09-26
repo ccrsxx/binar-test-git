cb83619a9b090c643ca49b911a4ef49a6b51f2c3
alifaaziz <alifabdulaziz24@gmail.com>
2024-09-26 21:47:30 +0700

    takeoff and flight

--- index.js
+++ index.js
@@ -22,3 +22,22 @@ class EngineError extends Error {
 function land() {
   console.log('Plane is landing');
 }
+
+function takeOff() {
+    if (Math.random() > 0.5) {
+        throw new TakeOffError('Cannot take off the flight');
+    }
+    console.log('Plane is taking off');
+}
+
+function flight(latitude) {
+    if (Math.random() > 0.75) {
+        throw new EngineError('Engine is not working');
+    }
+    // 3000 < latitude < 10000
+    if (latitude > 10000 || latitude < 3000) {
+        console.log('Plane is flying at latitude: ', latitude);
+        throw new FlightError('Cannot fly at this latitude');
+    }
+    console.log('Flying at latitude: ', latitude);
+}
