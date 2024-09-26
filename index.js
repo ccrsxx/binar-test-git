f669bbf43a17f22b77dfbad71e4274c3efe1cc2d
alifaaziz <alifabdulaziz24@gmail.com>
2024-09-26 21:50:18 +0700

    land and simulation

--- index.js
+++ index.js
@@ -41,3 +41,40 @@ function flight(latitude) {
     }
     console.log('Flying at latitude: ', latitude);
 }
+
+function land() {
+    console.log('Plane is landing');
+}
+
+function planeSimulation(fly, latitude) {
+    try {
+        if (fly === 0) {
+            takeOff();
+        }
+        while (fly < 3) {
+            flight(latitude);
+            latitude += Math.floor(Math.random() * 1000);
+            fly++;
+        }
+        land();
+    } catch (error) {
+        if (error instanceof TakeOffError) {
+            console.log(error.message);
+        } else if (error instanceof FlightError) {
+            latitude = 9000;
+            if (fly < 3) {
+                fly++;
+                console.log('Trying to fly again');
+                planeSimulation(fly, latitude);
+            }
+        } else if (error instanceof EngineError) {
+            console.log('Plane is crashing');
+        } else {
+            console.log('Flight simulation failed');
+        }
+    }
+}
+
+let latitude = Math.floor(Math.random() * 10000);
+let fly = 0;
+planeSimulation(0, 9000);
